8ca084eb52641b9e40a272725ff41309cb52c10e
Vadim Savin <savinvadim1312@gmail.com>
2023-06-16 19:08:29 +0100

    Date picker

--- App.tsx
+++ App.tsx
@@ -9,6 +9,7 @@ import AppleHealthKit, {
 } from 'react-native-health';
 import { useEffect, useState } from 'react';
 import useHealthData from './src/hooks/useHealthData';
+import { AntDesign } from '@expo/vector-icons';
 
 const permissions: HealthKitPermissions = {
   permissions: {
@@ -24,10 +25,36 @@ const permissions: HealthKitPermissions = {
 const STEPS_GOAL = 10_000;
 
 export default function App() {
-  const { steps, flights, distance } = useHealthData(new Date(2023, 5, 16));
+  const [date, setDate] = useState(new Date());
+  const { steps, flights, distance } = useHealthData(date);
+
+  const changeDate = (numDays) => {
+    const currentDate = new Date(date); // Create a copy of the current date
+    // Update the date by adding/subtracting the number of days
+    currentDate.setDate(currentDate.getDate() + numDays);
+
+    setDate(currentDate); // Update the state variable
+  };
 
   return (
     <View style={styles.container}>
+      <View style={styles.datePicker}>
+        <AntDesign
+          onPress={() => changeDate(-1)}
+          name="left"
+          size={20}
+          color="#C3FF53"
+        />
+        <Text style={styles.date}>{date.toDateString()}</Text>
+
+        <AntDesign
+          onPress={() => changeDate(1)}
+          name="right"
+          size={20}
+          color="#C3FF53"
+        />
+      </View>
+
       <RingProgress
         radius={150}
         strokeWidth={50}
@@ -57,5 +84,17 @@ const styles = StyleSheet.create({
     gap: 25,
     flexWrap: 'wrap',
     marginTop: 100,
+  },
+  datePicker: {
+    alignItems: 'center',
+    padding: 20,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  date: {
+    color: 'white',
+    fontWeight: '500',
+    fontSize: 20,
+    marginHorizontal: 20,
   },
 });
