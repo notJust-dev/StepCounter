09f393b3e4b1e6f92e9c413c1d6c833ef08236fd
Vadim Savin <savinvadim1312@gmail.com>
2023-06-16 15:40:17 +0100

    use flex gap

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ type ValueProps = {
 };
 
 const Value = ({ label, value }: ValueProps) => (
-  <View style={styles.valueContainer}>
+  <View>
     <Text style={styles.label}>{label}</Text>
     <Text style={styles.value}>{value}</Text>
   </View>
@@ -18,11 +18,11 @@ export default function App() {
     <View style={styles.container}>
       <Text>Hello world!</Text>
 
-      <View style={{ flexDirection: 'row' }}>
+      <View style={styles.values}>
         <Value label="Steps" value="1219" />
         <Value label="Distance" value="0,75 km" />
+        <Value label="Flights Climbed" value="12" />
       </View>
-      <Value label="Flights Climbed" value="12" />
 
       <StatusBar style="auto" />
     </View>
@@ -36,16 +36,17 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     padding: 12,
   },
-  valueContainer: {
-    marginRight: 50,
-    marginVertical: 10,
+  values: {
+    flexDirection: 'row',
+    gap: 25,
+    flexWrap: 'wrap',
   },
   label: {
     color: 'white',
     fontSize: 20,
   },
   value: {
-    fontSize: 35,
+    fontSize: 45,
     color: '#AFB3BE',
     fontWeight: '500',
   },
